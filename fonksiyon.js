3c7fb0ef14d0297e4a2c8b45b4d1cc5ea52435a7
ipekcoskunn <ipekcoskun59@gmail.com>
2020-09-04 13:09:30 +0300

    project pertama

---
+++ fonksiyon.js
@@ -0,0 +1,60 @@
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+	
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+
+
+/*Kısım6_1 */
+function menubel(evt, cityName) {
+  var i, tabcontent1, tablinks;
+  tabcontent1 = document.getElementsByClassName("tabcontent1");
+  for (i = 0; i < tabcontent1.length; i++) {
+    tabcontent1[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+	
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function Gecis(evt, cityName) {
+  var i, tabdeneme, tablinks;
+  tabdeneme = document.getElementsByClassName("tabdeneme");
+  for (i = 0; i < tabdeneme.length; i++) {
+    tabdeneme[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
+
+
